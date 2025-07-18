b1e8bfbc6c3086eb2884a1f182d8c830ee285814
ChristopherL <anbchris@gmail.com>
2025-07-17 21:16:43 -0400

    adding paddle and ball move logic to sockets and re-structuring code

--- server.js
+++ server.js
@@ -1,32 +1,15 @@
-const express = require('express');
 const http = require('http');
-const path = require('path');
 const socketIo = require('socket.io');
+const sockets = require('./sockets');
 
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
+const apiServer = require('./api');
+const httpServer = http.createServer(apiServer);
+const socketServer = socketIo(httpServer);
 
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, '/')));
-
-let readyPlayerCount = 0;
-
-io.on('connection', (socket) => {
-    console.log(`A user connected as...`, socket.id);
-
-    socket.on('ready', () => {
-        console.log('Player ready', socket.id);
-
-        readyPlayerCount++;
-
-        if (readyPlayerCount === 2) {
-            io.emit('startGame', socket.id);
-        }
-    });
-});
-
-server.listen(PORT, () => {
+httpServer.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
 });
+
+sockets.listen(socketServer);
---
+++ sockets.js
@@ -0,0 +1,34 @@
+let readyPlayerCount = 0;
+
+function listen(io) {
+    io.on('connection', (socket) => {
+        console.log(`A user connected as...`, socket.id);
+        
+        socket.on('ready', () => {
+            console.log('Player ready', socket.id);
+            
+            readyPlayerCount++;
+            
+            console.log('Ready Player Count: ', readyPlayerCount);
+            if (readyPlayerCount >= 2 && readyPlayerCount % 2 === 0) {
+                io.emit('startGame', socket.id);
+            }
+        });
+    
+        socket.on('paddleMove', (paddleData) => {
+            socket.broadcast.emit('paddleMove', paddleData);
+        });
+    
+        socket.on('ballMove', (ballData) => {
+            socket.broadcast.emit('ballMove', ballData);
+        });
+    
+        socket.on('disconnect', (reason) => {
+            console.log(`Client ${socket.id} disconnected: ${reason}`);
+        });
+    });
+}
+
+module.exports = { 
+    listen,
+};
